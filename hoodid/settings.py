57092f8d12a005030cc783d2d772cbcd07e05dcb
Charles Tenório da Silva <charlestenorios@gmail.com>
2019-09-16 15:54:45 -0300

    altercao de para S3

--- hoodid/settings.py
+++ hoodid/settings.py
@@ -227,3 +227,22 @@ SEXO_CHOICES = (
     ('Feminino', 'Feminino'),
     ('Outros', 'Outros'),
 )
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'hoodidfile'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
+AWS_LOCATION = 'static'
+STATICFILES_DIRS = ['estaticos']
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'

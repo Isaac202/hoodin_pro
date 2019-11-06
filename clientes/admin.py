ee7af3618024d9310430b76ec051804f3b10c840
CharlesTenorio <charlestenorios@gmail.com>
2019-11-05 23:55:00 -0300

    adm clientes

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Clientes
 
-admin.site.register(Clientes)
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sexo', 'data_nascimento', 'cnpjcpf', 'celular', 'telefone')
+    list_filter = ('tipo_pessoa',)
+    search_fields = ('nome', 'email')
+
+admin.site.register(Clientes, ClienteAdmin)
 
 
 # Register your models here.

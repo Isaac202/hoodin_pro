8e188a40eff53da3c5fac3ec12f6997d39433b18
Drummerzzz <jf.interatividade@gmail.com>
2019-11-19 14:55:06 -0300

    criando view de teste de envio de email na home

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from home.views import IndexView
+from home.views import IndexView, enviar_mail_teste
 
 app_name = "home"
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('send-mail', enviar_mail_teste)
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth import logout
+from django.http import HttpResponse
+from tools.tests import send_mail as s
 
 class IndexView(TemplateView):
     template_name = 'home/index.html'
@@ -10,3 +12,8 @@ class IndexView(TemplateView):
 def sair(request):
     logout(request)
     return redirect('login')
+
+
+def enviar_mail_teste(request):
+    s()
+    return HttpResponse("email enviado")

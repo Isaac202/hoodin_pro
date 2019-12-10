aa5afe22a3ccbb5a25c526a13207954bb60bd9e9
Drummerzzz <jf.interatividade@gmail.com>
2019-12-10 15:55:54 -0300

    refazendo relatorio de compras

--- clientes/admin.py
+++ clientes/admin.py
@@ -3,22 +3,39 @@ from .models import Clientes
 from tools.render import Render
 from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 
+
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'id', 'sexo', 'data_nascimento',
                     'cnpjcpf', 'celular', 'telefone')
-    list_filter = ('tipo_pessoa', 'sexo','data_nascimento', 'data_cadastro',
-         ('data_nascimento', DateRangeFilter), ('data_cadastro', DateRangeFilter),
-    )
+    list_filter = ('tipo_pessoa', 'sexo', 'data_nascimento', 'data_cadastro',
+                   ('data_nascimento',
+                    DateRangeFilter), ('data_cadastro', DateRangeFilter),
+                   )
     search_fields = ('nome', 'id_usuario__username', 'cnpjcpf',)
     # filter_horizontal = ('atuacao',)
     autocomplete_fields = ('id_usuario', 'atuacao')
-    actions = ['gera_pdf',]
+    actions = ['gera_pdf_anailitico', 'gera_pdf_sintetico']
 
-    def gera_pdf(set, request, queryset):
+    def gera_pdf_anailitico(set, request, queryset):
         if queryset:
-            return Render.render_to_pdf(request, queryset)
+            return Render.render_to_pdf(
+                request,
+                queryset,
+                description="Relatório de clientes Analítico"
+            )
 
-    gera_pdf.short_description = "Gerar PDF"
+    def gera_pdf_sintetico(set, request, queryset):
+        if queryset:
+            return Render.render_to_pdf(
+                request,
+                queryset,
+                template="tools/pdf_clientes_sintetico.html",
+                description="Relatório de clientes Sintético"
+            )
+
+    gera_pdf_anailitico.short_description = "Relatório Analítico"
+    gera_pdf_sintetico.short_description = "Relatório Sintético"
+
     # def gera_xls(set, request, queryset):
     #     if queryset:
     #         return Render.render_to_xls(request=request, queryset=queryset)

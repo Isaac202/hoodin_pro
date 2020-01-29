30184c357dd9de15e3b9dd459974f8b7ce5a0717
Drummerzzz <jf.interatividade@gmail.com>
2020-01-29 12:56:12 -0300

    arrumando percentual da obra

--- registros_coautores/forms.py
+++ registros_coautores/forms.py
@@ -6,3 +6,10 @@ class CoautoresForm(forms.ModelForm):
     class Meta:
         model = Coautores
         exclude = ("arquivo",)
+        
+    def clean_percentual_obra(self):
+        data = self.cleaned_data["percentual_obra"]
+        if data > 0:
+            return data
+        raise forms.ValidationError('Porcentagem deve ser maior que zero')
+    

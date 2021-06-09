a7922424e4d8802c72a6c6b61eb6f89b8a3ad71b
Volney Casas <volneyrock@gmail.com>
2021-06-08 21:00:22 -0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ USER=ubuntu
 # Endereço local que o gunicorn irá rodar
 ADDRESS=127.0.0.1:8000
 # Ativando ambiente virtual e executando o gunicorn para este projeto
-source /home/ubuntu/webapps/env/bin/activate
+source /home/ubuntu/env/bin/activate
 cd /home/ubuntu/webapps/
 #Executa o gunicorn
 test -d $LOGDIR || mkdir -p $LOGDIR
